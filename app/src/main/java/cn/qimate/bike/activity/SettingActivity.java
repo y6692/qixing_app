bf0d9a41ae0104818992766184ff3401892ed050
y6692 <y6692@126.com>
2019-03-19 14:57:10 +0800

    1.5.3新UI版完善27

--- app/src/main/java/cn/qimate/bike/activity/SettingActivity.java
+++ app/src/main/java/cn/qimate/bike/activity/SettingActivity.java
@@ -31,6 +31,7 @@ import cn.qimate.bike.R;
 import cn.qimate.bike.core.common.HttpHelper;
 import cn.qimate.bike.core.common.SharedPreferencesUrls;
 import cn.qimate.bike.core.common.UIHelper;
+import cn.qimate.bike.core.common.UpdateManager;
 import cn.qimate.bike.core.common.Urls;
 import cn.qimate.bike.core.widget.LoadingDialog;
 import cn.qimate.bike.model.ResultConsel;
@@ -48,69 +49,17 @@ public class SettingActivity extends SwipeBackActivity implements View.OnClickLi
     private LoadingDialog loadingDialog;
     private ImageView backImg;
     private TextView title;
-//    private TextView rightBtn;
 
-    private EditText nameEdit;
-    private TextView phoneNum;
-    private EditText nickNameEdit;
-    private RelativeLayout sexLayout;
-    private TextView sexText;
-    private RelativeLayout schoolLayout;
-    private TextView schoolText;
-    private RelativeLayout classLayout;
-    private TextView classEdit;
-    private EditText stuNumEdit;
-    private Button submitBtn;
-
-    private OptionsPickerView pvOptions;
-    private OptionsPickerView pvOptions1;
-    private OptionsPickerView pvOptions2;
-    private String sex = "";
-    private String school = "";
-
-    // 输入法
-    private List<SchoolListBean> schoolList;
-    static ArrayList<String> item1 = new ArrayList<>();
-    static ArrayList<String> item2 = new ArrayList<>();
-    static ArrayList<String> item3 = new ArrayList<>();
-
-    private Handler handler = new Handler() {
-        public void handleMessage(Message msg) {
-            // 三级联动效果
-            pvOptions.setPicker(item1);
-            pvOptions.setCyclic(false, false, false);
-            pvOptions.setSelectOptions(0, 0, 0);
-            sexLayout.setClickable(true);
-        };
-    };
-
-    private Handler handler1 = new Handler() {
-        public void handleMessage(Message msg) {
-            // 三级联动效果
-            pvOptions1.setPicker(item2);
-            pvOptions1.setCyclic(false, false, false);
-            pvOptions1.setSelectOptions(0, 0, 0);
-            schoolLayout.setClickable(true);
-        };
-    };
-    private Handler handler2 = new Handler() {
-        public void handleMessage(Message msg) {
-            // 三级联动效果
-            pvOptions2.setPicker(item3);
-            pvOptions2.setCyclic(false, false, false);
-            pvOptions2.setSelectOptions(0, 0, 0);
-            classLayout.setClickable(true);
-        };
-    };
+    private RelativeLayout cleanLayout, checkLayout, aboutUsLayout, questionLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.ui_person_setting);
+        setContentView(R.layout.activity_setting);
         context = this;
-        schoolList = new ArrayList<>();
         initView();
     }
+
 
     private void initView(){
 
@@ -118,135 +67,24 @@ public class SettingActivity extends SwipeBackActivity implements View.OnClickLi
         loadingDialog.setCancelable(false);
         loadingDialog.setCanceledOnTouchOutside(false);
 
-        // 选项选择器
-        pvOptions = new OptionsPickerView(context,false);
-        pvOptions1 = new OptionsPickerView(context,false);
-        pvOptions2 = new OptionsPickerView(context,false);
-
-        pvOptions.setTitle("选择学校");
-        pvOptions1.setTitle("选择性别");
-        pvOptions2.setTitle("选择年级");
-
         backImg = (ImageView) findViewById(R.id.mainUI_title_backBtn);
         title = (TextView) findViewById(R.id.mainUI_title_titleText);
-        title.setText("用户信息");
-//        rightBtn = (TextView) findViewById(R.id.mainUI_title_rightBtn);
-//        rightBtn.setText("变更手机");
+        title.setText("设置");
 
-        nameEdit = (EditText)findViewById(R.id.settingUI_name);
-        phoneNum = (TextView)findViewById(R.id.settingUI_phoneNum);
-        nickNameEdit = (EditText)findViewById(R.id.settingUI_nickName);
-        sexLayout = (RelativeLayout)findViewById(R.id.settingUI_sexLayout);
-        sexText = (TextView)findViewById(R.id.settingUI_sex);
-        schoolLayout = (RelativeLayout)findViewById(R.id.settingUI_schoolLayout);
-        schoolText = (TextView)findViewById(R.id.settingUI_school);
-        classLayout = (RelativeLayout)findViewById(R.id.settingUI_classLayout);
-        classEdit = (TextView)findViewById(R.id.settingUI_class);
-        stuNumEdit = (EditText)findViewById(R.id.settingUI_stuNum);
 
-        submitBtn = (Button)findViewById(R.id.settingUI_submitBtn);
-
-        if (schoolList.isEmpty() || item1.isEmpty()){
-            getSchoolList();
-        }
+        cleanLayout = (RelativeLayout) findViewById(R.id.personUI_bottom_cleanLayout);
+        checkLayout = (RelativeLayout) findViewById(R.id.personUI_bottom_checkLayout);
+        aboutUsLayout = (RelativeLayout) findViewById(R.id.personUI_bottom_aboutUsLayout);
+        questionLayout = (RelativeLayout) findViewById(R.id.personUI_bottom_questionLayout);
 
         backImg.setOnClickListener(this);
-//        rightBtn.setOnClickListener(this);
-        sexLayout.setOnClickListener(this);
-        schoolLayout.setOnClickListener(this);
-        classLayout.setOnClickListener(this);
-        submitBtn.setOnClickListener(this);
+        cleanLayout.setOnClickListener(this);
+        checkLayout.setOnClickListener(this);
+        aboutUsLayout.setOnClickListener(this);
+        questionLayout.setOnClickListener(this);
+
         initHttp();
 
-        new Thread(new Runnable() {
-
-            @Override
-            public void run() {
-                if (item2!= null && !item2.isEmpty() && 0 != item2.size()) {
-                    handler1.sendEmptyMessage(0x123);
-                    return;
-                }
-                if (!item2.isEmpty() || 0 != item2.size()) {
-                    item2.clear();
-                }
-                item2.add("男");
-                item2.add("女");
-                handler1.sendEmptyMessage(0x123);
-            }
-        }).start();
-
-        new Thread(new Runnable() {
-
-            @Override
-            public void run() {
-                if (item3!= null && !item3.isEmpty() && 0 != item3.size()) {
-                    handler2.sendEmptyMessage(0x123);
-                    return;
-                }
-                if (!item3.isEmpty() || 0 != item3.size()) {
-                    item3.clear();
-                }
-                item3.add("大一");
-                item3.add("大二");
-                item3.add("大三");
-                item3.add("大四");
-                item3.add("硕士生");
-                item3.add("博士生");
-                item3.add("教职工");
-                item3.add("其他");
-                handler2.sendEmptyMessage(0x123);
-            }
-        }).start();
-
-        // 设置默认选中的三级项目
-        // 监听确定选择按钮
-        pvOptions.setOnoptionsSelectListener(new OptionsPickerView.OnOptionsSelectListener() {
-
-            @Override
-            public void onOptionsSelect(int options1, int option2, int options3) {
-                school = item1.get(options1);
-                schoolText.setText(school);
-            }
-        });
-
-        pvOptions1.setOnoptionsSelectListener(new OptionsPickerView.OnOptionsSelectListener() {
-
-            @Override
-            public void onOptionsSelect(int options1, int option2, int options3) {
-                sex = item2.get(options1);
-                sexText.setText(item2.get(options1));
-            }
-        });
-
-        pvOptions2.setOnoptionsSelectListener(new OptionsPickerView.OnOptionsSelectListener() {
-
-            @Override
-            public void onOptionsSelect(int options1, int option2, int options3) {
-                classEdit.setText(item3.get(options1));
-            }
-        });
-
-        // 切换后将EditText光标置于末尾
-        CharSequence charSequence = stuNumEdit.getText();
-        if (charSequence instanceof Spannable) {
-            Spannable spanText = (Spannable) charSequence;
-            Selection.setSelection(spanText, charSequence.length());
-        }
-
-        if ("2".equals(SharedPreferencesUrls.getInstance().getString("iscert",""))){
-            nameEdit.setEnabled(false);
-            sexLayout.setEnabled(false);
-            schoolLayout.setEnabled(false);
-            classLayout.setEnabled(false);
-            stuNumEdit.setEnabled(false);
-
-        }else {
-            nameEdit.setEnabled(true);
-            sexLayout.setEnabled(true);
-            schoolLayout.setEnabled(true);
-            classLayout.setEnabled(true);
-            stuNumEdit.setEnabled(true);
-        }
     }
 
     @Override
@@ -258,102 +96,23 @@ public class SettingActivity extends SwipeBackActivity implements View.OnClickLi
                 scrollToFinishActivity();
                 break;
 
-            case R.id.settingUI_sexLayout:
-                pvOptions1.show();
+            case R.id.personUI_bottom_cleanLayout:
+                UIHelper.goToAct(context, MyPurseActivity.class);
                 break;
-            case R.id.settingUI_schoolLayout:
-                pvOptions.show();
+            case R.id.personUI_bottom_checkLayout:
+                UpdateManager.getUpdateManager().checkAppUpdate(context, true);
                 break;
-            case R.id.settingUI_classLayout:
-                pvOptions2.show();
+            case R.id.personUI_bottom_aboutUsLayout:
+                UIHelper.goWebViewAct(context, "关于我们", Urls.aboutUs);
                 break;
-            case R.id.settingUI_submitBtn:
-                String uid = SharedPreferencesUrls.getInstance().getString("uid","");
-                String access_token = SharedPreferencesUrls.getInstance().getString("access_token","");
-                if (uid == null || "".equals(uid) || access_token == null || "".equals(access_token)){
-                    Toast.makeText(context,"请先登录账号",Toast.LENGTH_SHORT).show();
-                    return;
-                }
-                String name = nameEdit.getText().toString().trim();
-                String nickName = nickNameEdit.getText().toString().trim();
-                String grade = classEdit.getText().toString().trim();
-                String stuNum = stuNumEdit.getText().toString().trim();
-                if(name == null || "".equals(name)){
-                    Toast.makeText(context,"请输入您的姓名",Toast.LENGTH_SHORT).show();
-                    return;
-                }
-                if(nickName == null || "".equals(nickName)){
-                    Toast.makeText(context,"请输入您的昵称",Toast.LENGTH_SHORT).show();
-                    return;
-                }
-                if(sex == null || "".equals(sex)){
-                    Toast.makeText(context,"请选择您的性别",Toast.LENGTH_SHORT).show();
-                    return;
-                }
-                if(school == null || "".equals(school)){
-                    Toast.makeText(context,"请选择您的学校",Toast.LENGTH_SHORT).show();
-                    return;
-                }
-                if(grade == null || "".equals(grade)){
-                    Toast.makeText(context,"请输入您的年级",Toast.LENGTH_SHORT).show();
-                    return;
-                }
-                if(stuNum == null || "".equals(stuNum)){
-                    Toast.makeText(context,"请输入您的学号",Toast.LENGTH_SHORT).show();
-                    return;
-                }
-                submit(uid,access_token,name,nickName,sex,school,grade,stuNum);
+            case R.id.personUI_bottom_questionLayout:
+                UIHelper.goToAct(context, ServiceCenterActivity.class);
                 break;
+
         }
     }
 
-    private void submit(String uid,String access_token,String realname,String nickname,
-                        String sex,String school,String grade,String stunum){
 
-        RequestParams params = new RequestParams();
-        params.put("uid",uid);
-        params.put("access_token",access_token);
-        params.put("realname",realname);
-        params.put("nickname",nickname);
-        params.put("sex",sex);
-        params.put("school",school);
-        params.put("grade",grade);
-        params.put("stunum",stunum);
-        HttpHelper.post(context, Urls.editUserinfo, params, new TextHttpResponseHandler() {
-            @Override
-            public void onStart() {
-                if (loadingDialog != null && !loadingDialog.isShowing()) {
-                    loadingDialog.setTitle("正在提交");
-                    loadingDialog.show();
-                }
-            }
-            @Override
-            public void onFailure(int statusCode, Header[] headers, String responseString, Throwable throwable) {
-                if (loadingDialog != null && loadingDialog.isShowing()){
-                    loadingDialog.dismiss();
-                }
-                UIHelper.ToastError(context, throwable.toString());
-            }
-
-            @Override
-            public void onSuccess(int statusCode, Header[] headers, String responseString) {
-                try {
-                    ResultConsel result = JSON.parseObject(responseString, ResultConsel.class);
-                    if (result.getFlag().equals("Success")) {
-                        Toast.makeText(context,"恭喜您,信息修改成功",Toast.LENGTH_SHORT).show();
-                        scrollToFinishActivity();
-                    } else {
-                        Toast.makeText(context,result.getMsg(),Toast.LENGTH_SHORT).show();
-                    }
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-                if (loadingDialog != null && loadingDialog.isShowing()){
-                    loadingDialog.dismiss();
-                }
-            }
-        });
-    }
 
 
     private void initHttp(){
@@ -386,16 +145,8 @@ public class SettingActivity extends SwipeBackActivity implements View.OnClickLi
                         ResultConsel result = JSON.parseObject(responseString, ResultConsel.class);
                         if (result.getFlag().equals("Success")) {
                             UserIndexBean bean = JSON.parseObject(result.getData(), UserIndexBean.class);
-                            nameEdit.setText(bean.getRealname());
-                            phoneNum.setText(bean.getTelphone());
-                            nickNameEdit.setText(bean.getNickname());
-                            sexText.setText(bean.getSex());
-                            schoolText.setText(bean.getSchool());
-                            classEdit.setText(bean.getGrade());
-                            stuNumEdit.setText(bean.getStunum());
+//                            nameEdit.setText(bean.getRealname());
 
-                            sex = bean.getSex();
-                            school = bean.getSchool();
                         } else {
                             Toast.makeText(context,result.getMsg(),Toast.LENGTH_SHORT).show();
                         }
@@ -413,60 +164,7 @@ public class SettingActivity extends SwipeBackActivity implements View.OnClickLi
         }
     }
 
-    /**
-     *
-     * 获取学校
-     * */
-    private void getSchoolList(){
 
-        HttpHelper.get(context, Urls.schoolList, new TextHttpResponseHandler() {
-
-            @Override
-            public void onStart() {
-                if (loadingDialog != null && !loadingDialog.isShowing()) {
-                    loadingDialog.setTitle("正在加载");
-                    loadingDialog.show();
-                }
-            }
-
-            @Override
-            public void onFailure(int statusCode, Header[] headers, String responseString, Throwable throwable) {
-                if (loadingDialog != null && loadingDialog.isShowing()){
-                    loadingDialog.dismiss();
-                }
-                UIHelper.ToastError(context, throwable.toString());
-            }
-
-            @Override
-            public void onSuccess(int statusCode, Header[] headers, String responseString) {
-                try {
-                    ResultConsel result = JSON.parseObject(responseString, ResultConsel.class);
-                    if (result.getFlag().equals("Success")) {
-                        JSONArray JSONArray = new JSONArray(result.getData());
-                        if (schoolList.size() != 0 || !schoolList.isEmpty()){
-                            schoolList.clear();
-                        }
-                        if (item1.size() != 0 || !item1.isEmpty()){
-                            item1.clear();
-                        }
-                        for (int i = 0; i < JSONArray.length();i++){
-                            SchoolListBean bean = JSON.parseObject(JSONArray.getJSONObject(i).toString(),SchoolListBean.class);
-                            schoolList.add(bean);
-                            item1.add(bean.getSchool());
-                        }
-                        handler.sendEmptyMessage(0x123);
-                    }else {
-                        Toast.makeText(context,result.getMsg(),Toast.LENGTH_SHORT).show();
-                    }
-                }catch (Exception e) {
-                    e.printStackTrace();
-                }
-                if (loadingDialog != null && loadingDialog.isShowing()){
-                    loadingDialog.dismiss();
-                }
-            }
-        });
-    }
 
     @Override
     public boolean onKeyDown(int keyCode, KeyEvent event) {
