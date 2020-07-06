ed2bca06f9863af22db9dff4557d13d18d4b604d
y6692@126.com <y6692509>
2020-07-06 17:13:21 +0800

    v2.0.11 完善1

--- app/src/main/java/cn/qimate/bike/activity/MainActivity.java
+++ app/src/main/java/cn/qimate/bike/activity/MainActivity.java
@@ -371,6 +371,7 @@ public class MainActivity extends BaseActivity implements View.OnClickListener, 
 //            }
 //        }
 
+
         mContext = this;
 
         mainFragment = new MainFragment();
