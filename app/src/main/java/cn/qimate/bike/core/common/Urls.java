6103faf9962e6a432352e11614641f5dec887a16
y6692 <y6692@126.com>
2020-12-16 09:44:23 +0800

    7MA出行APP测试版本v2.0.17完善2

--- app/src/main/java/cn/qimate/bike/core/common/Urls.java
+++ app/src/main/java/cn/qimate/bike/core/common/Urls.java
@@ -12,8 +12,8 @@ public class Urls {
 	public static String HTTP = "https://";
 	public static String host = HTTP + "app.7mate.cn";
 //	public static String host2 = HTTP + "newmapi.7mate.cn/api";	    //https://newmapi.7mate.cn/api
-    public static String host2 = HTTP + "test-mapi.7mate.cn/api";
-//	public static String host2 = HTTP + "dev-mapi.7mate.cn/api";
+//    public static String host2 = HTTP + "test-mapi.7mate.cn/api";
+	public static String host2 = HTTP + "dev-mapi.7mate.cn/api";
 
 //	public static String host = HTTP + "web1.7mate.cn";
 //	public static String host = HTTP + "test.7mate.cn";
