83f352a43c4693dd98e13828f93a4672a2201066
y6692 <y6692@126.com>
2021-01-27 14:41:29 +0800

    7MA出行APP生产版本v2.0.20：type13 大小：18.7MB 1. 新增type13单车语音智能锁； 2. 修复已知问题。

--- app/src/main/java/cn/qimate/bike/core/common/Urls.java
+++ app/src/main/java/cn/qimate/bike/core/common/Urls.java
@@ -21,7 +21,6 @@ public class Urls {
 //  public static String host = HTTP + "192.168.206.10:8345";
 //  public static String host2 = HTTP + "dev-mapi.7mate.cn/api";
 
-
 	//注册登录接口、退出登录接口delete
 	public static String authorizations = host2 + "/authorizations";
 	//获取验证码接口
